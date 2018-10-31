4814f2a2df75332889c6a3f60bf07b730b0c39db
Matic Zavadlal <matic.zavadlal@gmail.com>
2018-10-31 23:55:56 +0100

    first tests

---
+++ server/src/__tests__/index.test.ts
@@ -0,0 +1,8 @@
+describe('Emma sets up', () => {
+  test('Sends PR on new installed repository', async () => {
+    fail()
+  })
+  test('Watches for configuration', async () => {
+    fail()
+  })
+})
