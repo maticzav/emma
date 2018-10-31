604ccb7317381453634b0e8175b3abac7115867b
Matic Zavadlal <matic.zavadlal@gmail.com>
2018-10-31 23:14:38 +0100

    model

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,10 @@
 import { Application } from 'probot'
 
+// Webhooks
+
 export = (app: Application) => {
-  app.on('issues.opened', async context => {})
+  app.on('push', async context => {})
+  app.on('installation_repositories', async context => {})
 }
+
+// Helper functions
--- server/src/mongoose.ts
+++ server/src/mongoose.ts
@@ -1,11 +1,35 @@
-import { Schema, Types, model } from 'mongoose'
+import { Schema, Types, model, connect, connection } from 'mongoose'
+
+// Config
+
+if (!process.env.MONGOOSE_URI) {
+  throw new Error(`Missing configuration MONGOOSE_URI.`)
+}
+
+connect(process.env.MONGOOSE_URI)
+
+connection.once('open', () => {
+  console.log(`Connected to MongoDB`)
+})
+
+connection.on('error', err => {
+  console.error(`Error with MongoDB:`, err)
+})
+
+// Schema
 
 const boilerplateSchema = new Schema({
   id: { type: Types.ObjectId, unique: true, auto: true },
   name: { type: String, unique: true },
   description: { type: String, unique: true },
-  repository: { type: String, unique: false },
-  branch: { type: String, unique: true },
+  repository: {
+    uri: { type: String, unique: false },
+    branch: { type: String, unique: true },
+    path: { type: String, unique: false },
+  },
+  // analytics: {
+  //   downloads: { type: Number },
+  // },
 })
 
 export const Boilerplate = model('Boilerplate', boilerplateSchema)
